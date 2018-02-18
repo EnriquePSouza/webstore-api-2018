95de9f3e90be9e97c1e778094d57d52f29e91972
unknown <enriquepdsz@gmail.com>
2018-02-18 15:55:41 -0300

    Entitites lists - Functional changes

--- WebStore/WebStore.Domain/StoreContext/Entities/Customer.cs
+++ WebStore/WebStore.Domain/StoreContext/Entities/Customer.cs
@@ -1,10 +1,12 @@
 using System.Collections.Generic;
+using System.Linq;
 using WebStore.Domain.StoreContext.ValueObjects;
 
 namespace WebStore.Domain.StoreContext.Entities 
 {
     public class Customer 
     {
+        private readonly IList<Address> _addresses;
         public Customer (Name name, Document document,
             Email email, string phone) 
         {
@@ -12,13 +14,18 @@ namespace WebStore.Domain.StoreContext.Entities
             Document = document;
             Email = email;
             Phone = phone;
-            Addresses = new List<Address>();
+            _addresses = new List<Address>();
         }
         public Name Name { get; private set; }
         public Document Document { get; private set; }
         public Email Email { get; private set; }
         public string Phone { get; private set; }
-        public IReadOnlyCollection<Address> Addresses { get; private set; }
+        public IReadOnlyCollection<Address> Addresses => _addresses.ToArray();
+
+        public void AddAddress(Address address)
+        {
+            _addresses.Add(address);
+        }
 
         public override string ToString()
         {
