b919ecb1d236af93435d18f19ffcf95a9c727a51
unknown <enriquepdsz@gmail.com>
2018-02-18 07:18:25 -0300

    Adding Entities

---
+++ WebStore/WebStore.Domain/StoreContext/Product.cs
@@ -0,0 +1,12 @@
+namespace WebStore.Domain.StoreContext 
+{
+    public class Product 
+    {
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public string Image { get; set; }
+        public string Price { get; set; }
+        // Estoque Simples - Quantidade em estoque armazenada direto no produto.
+        public string QuantityOnHand { get; set; }
+    }
+}
