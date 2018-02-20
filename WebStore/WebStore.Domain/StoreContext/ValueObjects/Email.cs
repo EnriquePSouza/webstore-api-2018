dafbea24c92fce75824818fda9daf17a5ee1ae35
unknown <enriquepdsz@gmail.com>
2018-02-20 08:57:36 -0300

    Adding Validation Notifications

--- WebStore/WebStore.Domain/StoreContext/ValueObjects/Email.cs
+++ WebStore/WebStore.Domain/StoreContext/ValueObjects/Email.cs
@@ -1,14 +1,22 @@
+using FluentValidator;
+using FluentValidator.Validation;
+
 namespace WebStore.Domain.StoreContext.ValueObjects
 {
-    public class Email
+    public class Email : Notifiable
     {
         public Email(string address)
         {
             Address = address;
+
+            AddNotifications(new ValidationContract()
+                .Requires()
+                .IsEmail(Address, "Email", "O E-mail é inválido")
+            );
         }
         public string Address { get; private set; }
 
-        public override string ToString() 
+        public override string ToString()
         {
             return Address;
         }
